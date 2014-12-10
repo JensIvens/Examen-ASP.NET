7061326f6876c3e540062ed4fc17dfaad1db403e
Jens Ivens <jens.ivens@hotmail.com>
2014-12-10 13:43:26 +0100

    Start project

---
+++ ExamenYannickJens/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExamenYannickJens.Startup))]
+namespace ExamenYannickJens
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
